04b9fe07b44fbddc86a112eda273286890b5637f
afefhallajow <afefhallajow@gmail.com>
2023-09-06 13:17:45 +0300

    Modules

---
+++ bootstrap/cache/recipe_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Recipe\\Providers\\RecipeServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Recipe\\Providers\\RecipeServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
